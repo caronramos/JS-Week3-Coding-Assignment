122f20cf1480a53dfbbcf1f39d94cf80355e4137
Caron Ramos <98505172+caronramos@users.noreply.github.com>
2022-02-20 20:33:20 -0800

    Update Week3CodingHW.js

--- Week3CodingHW.js
+++ Week3CodingHW.js
@@ -1,6 +1,22 @@
+
+
+
 //********
 //	Q#3
 //********
 var array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 var lastElement = array[array.length - 1];
 console.log("The last element in the array is " + lastElement);
+
+
+//********
+//	Q#4
+//********
+var array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+var firstElement = array[0];
+console.log("The first element in the array is " + firstElement);
+
+
+//********
+//	Q#5
+//********
